9caeea53162c3f2a85f0dedf7ed9acf66f5a9643
LiaizonPivo <103899460+LiaizonPivo@users.noreply.github.com>
2022-06-03 09:52:21 +0200

    Update jeu.hpp

--- jeu.hpp
+++ jeu.hpp
@@ -15,11 +15,15 @@ class Fantome
   protected:
     int posX, posY;
     Direction dir;
+    bool estMangeable;
 
   public:
-    Fantome();
-    int getPosX() const;
+    Fantome();                      // Constructeur d'un fantome
+    int getPosX() const;            // Retourne la position du fantome
     int getPosY() const;
+
+    bool getEstMangeable() const;   // Retourne l'état du fantome
+    void setEstMangeable(bool);     // Change l'état du fantome
 };
 
 class Jeu
@@ -28,6 +32,7 @@ class Jeu
     Case *terrain;
     int largeur, hauteur; // Nombre de cases en largeur et en hauteur
     int posPacmanX, posPacmanY;
+    int nbVies;
 
   public:
     std::list<Fantome> fantomes;
@@ -51,8 +56,20 @@ class Jeu
     // Retourne la position du Pacman
     int getPacmanX() const;
     int getPacmanY() const;
+    // Change la position du Pacman
+    void setPacmanX(int);
+    void setPacmanY(int);
 
-    // Retourne la case à une position donnée
+    //Change l'état des fantomes
+    void rendFantomesMangeables();
+    void rendFantomesNonMangeables();
+
+    // Retourne le nombre de vies
+    int getNbVie() const;
+    // Change le nombre de vies
+    void setNbVie(int);
+
+    // Retourne la case à une position donnée (Mur/Vide)
     Case getCase(int, int) const;
 
     // Indique si la case à une position donnée existe et est vide
