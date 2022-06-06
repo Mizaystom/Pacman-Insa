55dd131a942e4fc1beaf39e5f64311383587797d
LiaizonPivo <103899460+LiaizonPivo@users.noreply.github.com>
2022-06-06 11:24:17 +0200

    Update jeu.hpp

--- jeu.hpp
+++ jeu.hpp
@@ -16,75 +16,70 @@ class Fantome
     int posX, posY;
     Direction dir;
     bool estMangeable;
-    //Compteur d'incrément de temps pour l'état mangeable
-    int compteur;
 
   public:
     Fantome();                      // Constructeur d'un fantome
     int getPosX() const;            // Retourne la position du fantome
     int getPosY() const;
 
-    bool getEstMangeable() const;   // Retourne l'état du fantome
-    void setEstMangeable(bool);     // Change l'état du fantome
+    bool getEstMangeable() const;   //Retourne l'état du fantome
+    void setEstMangeable(bool);     //Change l'état du fantome
 
-    int getCompteur() const;   // Retourne le numéro du compteur
-    void setCompteur(int);     // Change le numéro du compteur
 };
 
 class Jeu
 {
   protected:
+    //Attributs du terrain
     Case *terrain;
-    int largeur, hauteur; // Nombre de cases en largeur et en hauteur
+    int largeur, hauteur;
+
+    //Attribut du pacman
     int posPacmanX, posPacmanY;
-    int nbVies;
-    int score;
     Direction directionPacman, directionPacmanFuture;
+    int nbVies;
+
+    //Attributs de la partie
+    int score;
+    int compteur;   //Compteur d'incrément de temps pour l'état mangeable des fantomes
     bool finNiveau;
 
   public:
+    //Fantomes de la partie
     std::list<Fantome> fantomes;
 
   public:
-    Jeu();
+    Jeu();                          //Constructeur de Jeu
+    ~Jeu();                         //Destructeurs de Jeu
     Jeu(const Jeu &)=delete;
-    ~Jeu();
 
-    Jeu &operator=(const Jeu &)=delete;
+    Jeu &operator=(const Jeu &)=delete; //Surcharge d'opérateur
 
-    bool init();
-    void evolue();
-    void ajouter_fantome();
-    void supprimer_fantome();
+    bool init();                    //Initie une partie
+    void evolue();                  //Actualise la partie à chaque coup de timer
+    void ajouter_fantome();         //Ajoute un fantome à la liste de fantome du jeu
+    void supprimer_fantome();       //Supprime """""""""""""""""""""""""""""""""""""
 
-    // Retourne les dimensions (en nombre de cases)
-    int getNbCasesX() const;
+    int getNbCasesX() const;        //Retourne les dimensions (en nombre de cases)
     int getNbCasesY() const;
 
-    // Retourne la position du Pacman
-    int getPacmanX() const;
+    int getPacmanX() const;         //Retourne la position du Pacman
     int getPacmanY() const;
-    // Change la position du Pacman
-    void setPacmanX(int);
+
+    void setPacmanX(int);           //Change la position du Pacman
     void setPacmanY(int);
 
-    //Change l'état des fantomes
-    void rendFantomesMangeables();
-    void rendFantomesNonMangeables();
+    int getNbVie() const;           //Retourne le nombre de vies
+    void setNbVie(int);             //Change le nombre de vies
 
-    // Retourne le nombre de vies
-    int getNbVie() const;
-    // Change le nombre de vies
-    void setNbVie(int);
+    void setScore(int);             //Change le score
 
-    //Change le score
-    void setScore(int);
+    Case getCase(int, int) const;   //Retourne le type de case à une position donnée (Mur/Vide/Pion/Gros Pion)
 
-    // Retourne la case à une position donnée (Mur/Vide/Pion/Gros Pion)
-    Case getCase(int, int) const;
+    bool getFinNiveau() const;      //Indique si le niveau est terminé
 
-    //Retourne si le niveau est terminé
-    bool getFinNiveau() const;
+    int getCompteur() const;        //Retourne le numéro du compteur
+    void setCompteur(int);          //Change le numéro du compteur
 
     // Indique si la case à une position donnée existe et est vide
     // (Pacman ou les fantomes peuvent l'occuper)
@@ -95,7 +90,7 @@ class Jeu
     bool deplacePacman();
 
     //Set Direction pacman
-    void setDirectionPacman(Direction);
+    void setDirectionPacmanFuture(Direction);
     void setDirectionPacman();
 
     //Test de position
